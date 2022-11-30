302f70af03cf24cbf96de07b0b6a7c9e45ce4232
Anton Kachan <aa.kvchvn@gmail.com>
2022-11-30 15:11:43 +0300

    feat: implement fs/rename module

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,19 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import fsPromises from 'fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const rename = async () => {
-    // Write your code here 
+    const wrongFilePath = join(__dirname, 'files', 'wrongFilename.txt');
+    const properFilePath = join(__dirname, 'files', 'properFilename.md');
+
+    try {
+        await fsPromises.rename(wrongFilePath, properFilePath);
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await rename();
