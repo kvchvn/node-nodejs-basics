ca7b91c3034f0154630aa2a530185c1552dd4c49
Anton Kachan <aa.kvchvn@gmail.com>
2022-11-30 15:12:00 +0300

    feat: implement fs/read module

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { readFile } from 'fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const pathToFile = join(__dirname, 'files', 'fileToRead.txt');
+
+    try {
+        const fileContent = await readFile(pathToFile, { encoding: 'utf-8' });
+        console.log(fileContent);
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
