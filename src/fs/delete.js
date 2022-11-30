007592cc4a4c33658801086657e179980b3e195d
Anton Kachan <aa.kvchvn@gmail.com>
2022-11-30 15:11:49 +0300

    feat: implement fs/delete module

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { rm } from 'fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, 'files', 'fileToRemove.txt');
+
+    try {
+        await rm(filePath);
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await remove();
