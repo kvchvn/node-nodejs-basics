cc1d3b09372497f0405c0c4b74578f96af54ab21
Anton Kachan <aa.kvchvn@gmail.com>
2022-12-02 15:22:21 +0300

    feat: implement streams/transform module

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,25 @@
+import {pipeline, Transform} from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+    const reverse = new Transform({
+        transform(chunk, encoding, callback) {
+            const transformedChunk = chunk.toString().trim();
+            const reversedChunk = transformedChunk.split('').reverse().join('');
+            this.push(`${reversedChunk}\n`);
+            callback();
+        }
+    });
+
+    pipeline(
+        process.stdin,
+        reverse,
+        process.stdout,
+        (err) => {
+            if (err) {
+                throw err;
+            }
+        }
+    )
 };
 
 await transform();
