3dadc93944f491b23b170249bcfcd65fe720ec1c
Anton Kachan <aa.kvchvn@gmail.com>
2022-12-02 15:22:12 +0300

    feat: implement streams/write module

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { createWriteStream } from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+    const file = join(__dirname, 'files', 'fileToWrite.txt');
+    const ws = createWriteStream(file);
+
+    process.stdin.pipe(ws);
+
+    process.stdin.on('end', () => {
+        process.stdin.unpipe(ws);
+    });
 };
 
 await write();
