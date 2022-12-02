126e8f11c0bcb3db1f1dd76d93ef093f65eaf091
Anton Kachan <aa.kvchvn@gmail.com>
2022-12-02 15:22:49 +0300

    feat: implement zip/decompress module

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,31 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { createUnzip } from 'zlib';
+import { pipeline } from "stream";
+import { createWriteStream, createReadStream } from 'fs';
+import { rm } from 'fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const decompress = async () => {
-    // Write your code here 
+    const sourceArchive = join(__dirname, 'files', 'archive.gz');
+    const rs = createReadStream(sourceArchive);
+    const ws = createWriteStream(join(__dirname, 'files', 'fileToCompress.txt'));
+
+    const unzip = createUnzip();
+
+    pipeline(
+        rs,
+        unzip,
+        ws,
+        async (err) => {
+            if (err) {
+                throw err;
+            }
+            await rm(sourceArchive);
+        }
+    );
 };
 
 await decompress();
