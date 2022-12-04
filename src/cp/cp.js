2a4dc4f394bfd12d4013edec0cd6747fc9105756
Anton Kachan <aa.kvchvn@gmail.com>
2022-12-04 13:31:49 +0300

    feat: implement cp/cp module

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,13 @@
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import { fork } from 'child_process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childProcessPath = join(__dirname, 'files', 'script.js');
+    const child = fork(childProcessPath, args);
 };
 
-spawnChildProcess();
+spawnChildProcess(process.argv.slice(2));
