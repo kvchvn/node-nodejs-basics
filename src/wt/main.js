edd7ff7851367a8f4ebda83188991544d6563465
Anton Kachan <aa.kvchvn@gmail.com>
2022-12-04 13:31:15 +0300

    feat: implement wt/main, wt/worker modules

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,30 @@
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const performCalculations = async () => {
-    // Write your code here
+    const magicNumber = 10;
+    const workerFile = join(__dirname, 'worker.js');
+
+    const runWorkerThread = (filename, workerData) => {
+      return new Promise((resolve, reject) => {
+          const worker = new Worker(filename, { workerData });
+          worker.on('message', resolve);
+          worker.on('error', reject);
+      });
+    };
+
+    const promises = cpus().map((_, index) => {
+        return runWorkerThread(workerFile, magicNumber + index)
+            .then((data) => ({ status: 'resolved', data }))
+            .catch(() => ({ status: 'error', data: null }));
+    });
+
+    console.log(await Promise.all(promises));
 };
 
 await performCalculations();
