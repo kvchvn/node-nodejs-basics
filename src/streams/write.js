53943cd30805f43b007ae5ccbe538e9746d1f3c3
Anton Kachan <aa.kvchvn@gmail.com>
2025-04-28 12:10:15 +0300

    feat: implement streams-related tasks

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,25 @@
+import url from "node:url";
+import path from 'node:path';
+import { createWriteStream } from 'node:fs';
+
 const write = async () => {
-    // Write your code here 
+  const DIRNAME = 'files';
+  const FILENAME = 'fileToWrite.txt';
+
+  const __filename = url.fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const filePath = path.join(__dirname, DIRNAME, FILENAME);
+
+  const ws = createWriteStream(filePath);
+
+  console.log('Press Ctrl+C to finish the process.\n');
+
+  process.stdin
+    .on('error', (err) => {
+      console.log('Error occurred during the process:\n', err);
+    })
+    .pipe(ws);
 };
 
 await write();
